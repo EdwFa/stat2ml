a538966164b9ec9a8c18fdda55c78498c9bac439
EdwFa <efart@mail.ru>
2025-09-04 14:29:22 +0300

    update

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from ci.ci_demo import agresti_coull_interval
 if __name__ == '__main__':
     x = 98  # число успехов
     n = 100  # размер выборки
-    confidence = 0.90
+    confidence = 0.90 # уровень значимости
+
     ci = wilson_confidence_interval(x, n, confidence)
     print(f"Доверительный интервал (метод Уилсона): [{ci[0]:.3f}, {ci[1]:.3f}]")
     ci = clopper_pearson_interval(x, n, confidence)
